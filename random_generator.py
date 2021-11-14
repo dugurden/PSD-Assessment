47866e1dd6cf264bae0f64a94911f3b0d17d2e7d
dugurden <dugurden@gmail.com>
2021-11-14 09:11:52 +0100

    Added assert

--- random_generator.py
+++ random_generator.py
@@ -1,7 +1,11 @@
 def random_gen(amount, minRange, maxRange):
+    assert minRange < maxRange, "minRange needs to be smaller than maxRange"
+    assert amount > 0, "amount needs to be a positive integer"
     count = amount
     result = []
     while count > 0:
         result.append(random.randint(minRange, maxRange))
         count -= 1
+    assert len(result) == amount, "wrong number of results"
+    assert count == 0, "Code must be wrong"
     print(result)
