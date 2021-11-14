834d77bbba193e8ef560621a272490700640b066
dugurden <dugurden@gmail.com>
2021-11-14 09:13:39 +0100

    Added class

--- random_generator.py
+++ random_generator.py
@@ -1,11 +1,16 @@
-def random_gen(amount, minRange, maxRange):
-    assert minRange < maxRange, "minRange needs to be smaller than maxRange"
-    assert amount > 0, "amount needs to be a positive integer"
-    count = amount
-    result = []
-    while count > 0:
-        result.append(random.randint(minRange, maxRange))
-        count -= 1
-    assert len(result) == amount, "wrong number of results"
-    assert count == 0, "Code must be wrong"
-    print(result)
+class RandomNum:
+    def __init__(self, amount, minRange, maxRange):
+        self.amount = amount
+        self.minRange = minRange
+        self.maxRange = maxRange
+    def random_gen(cls):
+        assert cls.minRange < cls.maxRange, "minRange needs to be smaller than maxRange"
+        assert cls.amount > 0, "amount needs to be a positive integer"
+        count = cls.amount
+        result = []
+        while count > 0:
+            result.append(random.randint(cls.minRange, cls.maxRange))
+            count -= 1
+        assert len(result) == cls.amount, "wrong number of results"
+        assert count == 0, "Code must be wrong"
+        print(result)
